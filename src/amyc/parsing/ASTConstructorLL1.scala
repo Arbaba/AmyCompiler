57bd2f6d061517e4b36c558dffdbb145c40189fa
Boubacar Camara <bakarsadio@gmail.com>
2018-12-28 14:09:33 +0100

    Add ast and parsing files

---
+++ src/amyc/parsing/ASTConstructorLL1.scala
@@ -0,0 +1,457 @@
+package amyc
+package parsing
+
+import grammarcomp.parsing._
+import utils.Positioned
+import ast.NominalTreeModule._
+import Tokens._
+import amyc.ast.NominalTreeModule
+import ast.NominalTreeModule._
+
+// Implements the translation from parse trees to ASTs for the LL1 grammar,
+// that is, this should correspond to Parser.amyGrammarLL1.
+// We extend the plain ASTConstructor as some things will be the same -- you should
+// override whatever has changed. You can look into ASTConstructor as an example.
+class ASTConstructorLL1 extends ASTConstructor {
+
+  // TODO: Override methods from ASTConstructor as needed
+
+  /* ... */
+
+
+  // Important helper method:
+  // Because LL1 grammar is not helpful in implementing left associativity,
+  // we give you this method to reconstruct it.
+  // This method takes the left operand of an operator (leftopd)
+  // as well as the tree that corresponds to the operator plus the right operand (ptree)
+  // It parses the right hand side and then reconstruct the operator expression
+  // with correct associativity.
+  // If ptree is empty, it means we have no more operators and the leftopd is returned.
+  // Note: You may have to override constructOp also, depending on your implementation
+   def constructOpExpr(leftopd: Expr, ptree: NodeOrLeaf[Token]): Expr = {
+
+   ptree match {
+     case Node(_, List()) => //epsilon rule of the nonterminals
+       leftopd
+     case Node(sym ::= _, List(op, rightNode))
+       //if Set('OrExpr, 'AndExpr, 'EqExpr, 'CompExpr, 'AddExpr, 'MultExpr) contains sym =>
+       if Set('ExprTerm, 'OrTermList, 'AndTermList, 'EqTermList, 'LessTermList, 'Plus_MinusTermList, 'MUL_DIV_MODTermList) contains sym =>
+     rightNode match {
+         case Node(_, List(nextOpd, suf)) => // 'Expr? ::= Expr? ~ 'OpExpr,
+           val nextAtom = findAndUseExpr(nextOpd)
+           constructOpExpr(constructOp(op)(leftopd, nextAtom).setPos(leftopd),  suf)
+            // captures left associativity
+         case Node(_, List()) =>
+         leftopd
+       }
+   }
+
+
+ }
+
+  override def constructOp(ptree: NodeOrLeaf[Token]): (Expr, Expr) => Expr = {
+    ptree match {
+      case Node(_, List(Leaf(t))) =>
+        tokenToExpr(t)
+      case Node('Less ::=_, List(Leaf(LESSTHAN())))=>
+        LessThan
+      case Node('Less ::=_, List(Leaf(LESSEQUALS())))=>
+        LessEquals
+      case Node ('PLUS_MINUS ::=_ , List(Leaf(PLUS()))) =>
+        Plus
+      case Node ('PLUS_MINUS ::=_ , List(Leaf(MINUS()))) =>
+        Minus
+      case Node ('PLUS_MINUS ::=_ , List(Leaf(CONCAT())) )=>
+        Concat
+      case Node('MUL_DIV_MODTerm ::=_, List(Leaf(TIMES()))) =>
+        Times
+      case Node ('MUL_DIV_MODTerm ::=_ , List(Leaf(DIV()))) =>
+        Div
+      case Node('MUL_DIV_MODTerm ::=_, List(Leaf(MOD())) )=>
+        Mod
+      case Leaf(op) => tokenToExpr(op)
+    }
+  }
+
+
+   /*override def constructQname(pTree: NodeOrLeaf[Token]): (QualifiedName, Positioned) ={
+     pTree match {
+       case Node('QName ::= _, List(id, qnames)) =>
+         val (name, pos) = constructName(id)
+         val tail = constructQnames(qnames)
+         tail match {
+           case Some((string, position)) =>
+             (QualifiedName(Some(name), string), pos)
+           case None =>
+             (QualifiedName(None, name), pos)
+         }
+     }
+
+   }*/
+
+ def constructQnames(pTree: NodeOrLeaf[Token]): Option[(String, Positioned)] = {
+   pTree match {
+     case Node('Qnames ::= List(DOT(), 'Id), List(_, id) ) =>
+       Some(constructName(id))
+     case Node('Qnames ::= _, List()) =>
+       None
+   }
+ }
+
+
+  override def constructCase(pTree: NodeOrLeaf[Token]): MatchCase = {
+    pTree match {
+      case Node('Case ::= _, List(Leaf(ct), pat, _, expr)) =>
+        MatchCase(constructPattern(pat), constructExpr(expr)).setPos(ct)
+    }
+  }
+
+  override def constructPattern(pTree: NodeOrLeaf[Token]): Pattern = {
+    pTree match {
+      case Node('Pattern ::= List(UNDERSCORE()), List(Leaf(ut))) =>
+        WildcardPattern().setPos(ut)
+      case Node('Pattern ::= List('Literal), List(lit)) =>
+        val literal = constructLiteral(lit)
+        LiteralPattern(literal).setPos(literal)
+
+      case Node('Pattern ::= List('IdOrQnameWithPattern), List(child)) =>
+        constructIdOrQnameWPattern(child)
+
+
+    }
+  }
+
+  def constructIdOrQnameWPattern(ptree : NodeOrLeaf[Token]): Pattern = {
+    ptree match {
+      case Node('IdOrQnameWithPattern::= List('QName, 'OptPatCall), List(qname, optPatCall)) =>
+        optPatCall match {
+          case Node('OptPatCall::= _, List(Leaf(lparen), optPatterns, _)) =>
+            optPatterns match {
+              case Node('OptPatterns ::= _, List(patterns)) =>
+                CaseClassPattern(constructQname(qname)._1, constructPatterns(patterns))//constr(args
+              case Node('OptPatterns ::=_ , List())=>
+                CaseClassPattern(constructQname(qname)._1, Nil) // constr()
+            }
+          case Node('OptPatCall::= _, List()) =>
+            qname match {
+              case Node('QName::= _, List(id, tail)) =>
+                val (qn, pos) = constructQname(qname)
+                IdPattern(qn.name).setPos(pos) //Id simple
+            }
+
+        }
+
+    }
+  }
+
+  def constructPatterns(ptree : NodeOrLeaf[Token]): List[Pattern] = {
+    ptree match {
+      case Node('Patterns::=_, List(pattern, patternsList)) =>
+        patternsList match {
+          case Node('PatternList ::= List(COMMA(), 'Patterns ), List(Leaf(comma), patterns)) =>
+            constructPattern(pattern) :: constructPatterns(patterns)
+          case Node('PatternList ::= _, List()) =>
+            List(constructPattern(pattern))
+        }
+      case Node('Patterns::=_, List()) =>
+        Nil
+    }
+  }
+
+
+
+  override def constructExpr(ptree: NodeOrLeaf[Token]): NominalTreeModule.Expr = {
+
+   ptree match {
+     case Node('Expr ::= List(VAL() , 'Param ,EQSIGN() , 'ExprTerm , SEMICOLON() ,'Expr),
+               List(Leaf(vall), par, Leaf(_), exprTerm, Leaf(_), expr)) =>
+       val paramDef = constructParam(par)
+       Let(paramDef, constructExprTerm(exprTerm), constructExpr(expr)).setPos(vall)//SET POS?????
+
+     case Node('Expr ::= List('ExprTerm, 'ExprTail), List(term, tail)) =>
+       tail match {
+         case Node('ExprTail ::= List(SEMICOLON(), 'Expr), List(Leaf(pos), expr)) =>
+           Sequence(constructExprTerm(term), constructExpr(expr))
+         case Node('ExprTail ::= _, List()) =>
+           constructExprTerm(term)
+       }
+   }
+ }
+  def findAndUseExpr(ptree: NodeOrLeaf[Token]): NominalTreeModule.Expr ={
+    ptree match {
+      case Node(exprtype ::=_,_ ) =>
+        exprtype match {
+          case 'Expr => constructExpr(ptree)
+          case 'ExprTerm => constructExprTerm(ptree)
+          case 'OrTerm => constructOrTerm(ptree)
+          case 'AndTerm => constructAndTerm(ptree)
+          case 'EqTerm => constructEqTerm(ptree)
+          case 'LessTerm => constructLessTerm(ptree)
+          case 'Plus_MinusTerm => constructPlusMinusTerm(ptree)
+          case 'MUL_DIV_MODTerm => constructMulDivModTerm(ptree)
+          case 'FinalTerm => constructFinalTerm(ptree)
+        }
+    }
+  }
+
+   def constructExprTerm(ptree: NodeOrLeaf[Token]): NominalTreeModule.Expr = {
+    ptree match {
+      case Node('ExprTerm ::= List('OrTerm, 'OptMatch), List(scrut, optMatch)) =>
+        optMatch match {
+          case Node('OptMatch ::= List(MATCH(), LBRACE(), 'Cases ,RBRACE()), List(Leaf(matchh),_, cases,_)) =>
+            Match(constructOrTerm(scrut), constructCases(cases))//construct cases
+          case Node('OptMatch ::= _, List()) =>
+            constructOrTerm(scrut)
+        }
+    }
+  }
+
+  def constructCasesTail(ptree: NodeOrLeaf[Token]): List[NominalTreeModule.MatchCase] = {
+    ptree match {
+      case Node('CasesTail ::= List('Cases), List(cases)) => constructCases(cases)
+      case Node('CasesTail ::= _, List()) => Nil
+    }
+  }
+
+  def constructCases(ptree: NodeOrLeaf[Token]): List[NominalTreeModule.MatchCase] = {
+    ptree match {
+      case Node('Cases ::= List('Case, 'CasesTail), List(casee, tail)) =>
+        super.constructCase(casee) :: constructCasesTail(tail)
+    }
+  }
+  def constructOrTerm(ptree: NodeOrLeaf[Token]): NominalTreeModule.Expr = {
+    ptree match  {
+      case Node('OrTerm ::=_, List(andterm, ortermlist)) =>
+        ortermlist match {
+          case Node('OrTermList ::= (OR() ::_), List(or,orterm)) =>
+            constructOpExpr(constructAndTerm(andterm), ortermlist)
+          case Node(_, List()) =>
+            constructAndTerm(andterm)
+        }
+    }
+  }
+
+  def constructAndTerm(ptree: NodeOrLeaf[Token]): NominalTreeModule.Expr = {
+    ptree match  {
+      case Node('AndTerm ::=_, List(eqterm, andtermlist)) =>
+        andtermlist match {
+          case Node('AndTermList ::= (AND() :: _), List(and,andterm)) =>
+            //constructOpExpr(and)(eqterm, andterm)
+            constructOpExpr(constructEqTerm(eqterm), andtermlist)
+          case Node(_, List()) =>
+            constructEqTerm(eqterm)
+        }
+    }
+  }
+
+  def constructEqTerm(ptree: NodeOrLeaf[Token]): NominalTreeModule.Expr = {
+    ptree match  {
+      case Node('EqTerm ::=_, List(lessterm, eqtermlist)) =>
+        eqtermlist match {
+          case Node('EqTermList ::= List(EQUALS(),'EqTerm), List(eq,eqterm)) =>
+            constructOpExpr(constructLessTerm(lessterm), eqtermlist)
+          case Node(_, List()) =>
+            constructLessTerm(lessterm)
+        }
+    }
+  }
+
+  def constructLessTerm(ptree: NodeOrLeaf[Token]): NominalTreeModule.Expr = {
+    ptree match  {
+      case Node('LessTerm ::=_, List(plusminusterm, lesstermlist)) =>
+        lesstermlist match {
+          case Node('LessTermList ::= List('LESS ,_), List(less,lessterm)) =>
+            less match {
+              case Node('LESS ::= _, List(ltorless)) =>
+                constructOpExpr(constructPlusMinusTerm(plusminusterm), lesstermlist)
+            }
+          case Node(_, List()) =>
+            constructPlusMinusTerm(plusminusterm)
+        }
+    }
+  }
+
+  def constructPlusMinusTerm(ptree: NodeOrLeaf[Token]): NominalTreeModule.Expr = {
+    ptree match  {
+      case Node('Plus_MinusTerm ::= List('MUL_DIV_MODTerm,'Plus_MinusTermList), List(muldivmodterm, plusminustermlist)) =>
+        plusminustermlist match {
+          case Node('Plus_MinusTermList ::= List('PLUS_MINUS,_), List(plusminus,plusminusterm)) =>
+            plusminus match {
+              case Node('PLUS_MINUS ::= _, List(op)) =>
+                constructOpExpr(constructMulDivModTerm(muldivmodterm), plusminustermlist)
+            }
+          case Node(_, List()) =>
+            constructMulDivModTerm(muldivmodterm)
+        }
+    }
+  }
+
+
+def constructMulDivModTerm(ptree: NodeOrLeaf[Token]): NominalTreeModule.Expr = {
+  ptree match  {
+    case Node('MUL_DIV_MODTerm  ::= List('FinalTerm, 'MUL_DIV_MODTermList), List(finalterm, muldivmodtermlist)) =>
+      muldivmodtermlist match {
+        case Node('MUL_DIV_MODTermList::= List('MUL_DIV_MOD ,_), List(mdv, muldivmodterm)) =>
+          mdv match {
+            case Node('MUL_DIV_MOD ::= _, List(op)) =>
+              constructOpExpr(constructFinalTerm(finalterm),muldivmodtermlist )
+          }
+        case Node(_, List()) =>
+          constructFinalTerm(finalterm)
+      }
+  }
+}
+// Need to add  Minus
+def constructFinalTerm(ptree: NodeOrLeaf[Token]): NominalTreeModule.Expr = {
+ // println(ptree)
+  ptree match {
+    case Node('FinalTerm ::= List('LiteralNoEmptyPar), List(lit)) =>
+      constructLiteral(lit)
+    case Node('FinalTerm ::= List('If), List(iff)) =>
+      constructIf(iff)
+    case Node('FinalTerm ::= ('Error ::_), List(err)) =>
+      constructError(err)
+    case Node('FinalTerm ::= ( 'Id :: 'OptCall ::_), List(name, optcall)) =>
+      optcall match {
+        case Node('OptCall ::= _, List()) =>
+          val (id,pos) = constructName(name)
+          Variable(id).setPos(pos)
+        case Node('OptCall ::= ('OptForQname :: LPAREN() :: 'Args :: RPAREN() :: _), List(optqname, _, par, _)) =>
+          val args = constructList(par, constructExpr, hasComma = true)
+          solveOptQname(optqname) match {
+            case None =>  //no module
+              val (identifier, pos) = constructName(name)
+              Call(QualifiedName(None, identifier), args).setPos(pos)
+            case Some(Variable(identifier)) => //module
+              val (module, modulePos) = constructName(name)
+              Call(QualifiedName(Some(module), identifier), args).setPos(modulePos)
+          }
+      }
+
+    case Node('FinalTerm ::= List('EmptyParOrParExpr), List(unitOrExpr)) =>
+      constructEmptyParOrParExpr(unitOrExpr)
+   /* case Node('FinalTerm ::= ('EmptyParOrParExpr ::_), List(foo)) =>
+      constructEmptyParOrParExpr(foo)*/
+    case Node('FinalTerm ::= (BANG() :: 'FinalTerm ::_), List(Leaf(justForPos), finalTerm)) =>
+     Not(constructFinalTerm(finalTerm)).setPos(justForPos)
+    case Node('FinalTerm ::= (MINUS() :: 'FinalTerm ::_), List(Leaf(justForPos), finalTerm)) =>
+      Neg(constructFinalTerm(finalTerm)).setPos(justForPos)
+
+  }
+}
+
+  def constructEmptyParOrParExpr(ptree : NodeOrLeaf[Token]): NominalTreeModule.Expr = {
+    ptree match {
+      case Node('EmptyParOrParExpr ::= _, List(Leaf(lpar), optExpr,_) ) =>
+        constructOptExpr(optExpr)
+
+    }
+  }
+
+  def constructOptExpr(ptree : NodeOrLeaf[Token]): NominalTreeModule.Expr = {
+    ptree match {
+      case Node('OptExpr ::= List('Expr), List(expr)) =>
+        constructExpr(expr)
+      case Node('OptExpr ::= _, List()) =>
+        UnitLiteral()
+    }
+  }
+
+  def isEpsilon(ptree: NodeOrLeaf[Token]): Boolean = {
+    ptree match {
+      case Node(sym ::= _, List()) => true
+      case Node(_, _) => false
+      case Leaf(_) => false
+
+    }
+  }
+
+  def solveOptQname(ptree: NodeOrLeaf[Token]): (Option[Variable]) = {
+    ptree match {
+      case Node(sym ::= _, List()) => None
+      case Node('OptForQname ::= List(DOT(), 'Id), List(_, id)) =>
+        val (name, pos) = constructName(id)
+        Some(Variable(name).setPos(pos))
+    }
+
+  }
+
+  def constructIf(ptree: NodeOrLeaf[Token]): NominalTreeModule.Expr = {
+    ptree match {
+      case Node('If ::= List(IF() , LPAREN() , 'Expr , RPAREN() , LBRACE() , 'Expr , RBRACE() , ELSE() , LBRACE() , 'Expr , RBRACE()), List(Leaf(it), _, cond, _, _, thenn, _, _, _, elze, _)) =>
+        Ite(
+          constructExpr(cond),
+          constructExpr(thenn),
+          constructExpr(elze)
+        ).setPos(it)
+    }
+
+  }
+
+  def constructError(ptree: NodeOrLeaf[Token]): NominalTreeModule.Expr = {
+    ptree match {
+      case Node('Error ::= (ERROR() :: _ ::_::_::_), List(Leaf(ert), _, msg, _)) =>
+        Error(constructExpr(msg)).setPos(ert)
+    }
+
+  }
+  override def constructLiteral(pTree: NodeOrLeaf[Token]): Literal[_] = {
+    pTree match {
+      case Node('LiteralNoEmptyPar ::= List(INTLITSENT), List(Leaf(it@INTLIT(i)))) =>
+        IntLiteral(i).setPos(it)
+      case Node('LiteralNoEmptyPar ::= List(STRINGLITSENT), List(Leaf(st@STRINGLIT(s)))) =>
+        StringLiteral(s).setPos(st)
+      case Node('LiteralNoEmptyPar ::= _, List(Leaf(tt@TRUE()))) =>
+        BooleanLiteral(true).setPos(tt)
+      case Node('LiteralNoEmptyPar ::= _, List(Leaf(tf@FALSE()))) =>
+        BooleanLiteral(false).setPos(tf)
+      case Node('LiteralNoEmptyPar ::= _, List(Leaf(lp@LPAREN()), Leaf(RPAREN()))) =>
+        UnitLiteral().setPos(lp)
+      case _ => super.constructLiteral(pTree)
+    }
+  }
+  override def constructQname(pTree: NodeOrLeaf[Token]): (QualifiedName, Positioned) = {
+    pTree match {
+       case Node('QName ::= List('Id,'QNames), List(modOrId, qnames)) =>
+         qnames match {
+           case Node('QNames ::= List(DOT(), 'Id), List(Leaf(dot), id)) =>
+             val (module, pos) = constructName(modOrId)
+             val (name, _) = constructName(id)
+             (QualifiedName(Some(module), name), pos)
+           case Node('QNames ::= _, List()) =>
+             val (name, pos) = constructName(modOrId)
+             (QualifiedName(None, name), pos)
+         }
+      case Node('FinalTerm ::= ( 'Id :: 'OptForQname :: LPAREN() :: 'Args :: RPAREN()::_),  List(modOrId, qnames, _, args,_)) =>
+        qnames match {
+          case Node('OptForQname ::= (DOT():: 'Id:: _), List(_, id)) =>
+            val (module, pos) = constructName(modOrId)
+            val (name, _) = constructName(id)
+            (QualifiedName(Some(module), name), pos)
+          case Node('OptForQname ::= List(), _) =>
+            val (name, pos) = constructName(modOrId)
+            (QualifiedName(None, name), pos)
+        }
+    }
+
+  }
+
+  def constructExprList(pTree: NodeOrLeaf[Token]): List[NominalTreeModule.Expr] = {
+    pTree match {
+      case Node('ExprList ::= (COMMA() :: 'Expr :: 'ExprList :: _), (expr :: exprLists:: _)) =>
+        constructList(pTree, constructExpr)
+      case Node('ExprList ::= List(), _) =>
+        Nil
+    }
+  }
+
+  def constructArgs(pTree: NodeOrLeaf[Token]): List[NominalTreeModule.Expr] = {
+    pTree match {
+      case Node('Args ::= ('Expr :: 'ExprList :: _), (expr :: exprLists:: _)) =>
+        constructList(pTree, constructExpr)
+      case Node('ExprList ::= (COMMA() :: 'Expr :: 'ExprList::_), (Leaf(pos) :: expr :: exprList ::_)) =>
+        constructExpr(expr) :: constructExprList(exprList)
+    }
+  }
+}
+
---
+++ src/amyc/parsing/Parser.scala
@@ -0,0 +1,177 @@
+package amyc
+package parsing
+
+import grammarcomp.grammar.CFGrammar._
+import grammarcomp.grammar.GrammarDSL._
+import grammarcomp.grammar.GrammarUtils.InLL1
+import grammarcomp.grammar._
+import grammarcomp.parsing._
+import amyc.utils._
+import ast.NominalTreeModule._
+import Tokens._
+
+// The parser for Amy
+// Absorbs tokens from the Lexer and then uses grammarcomp to generate parse trees.
+// Defines two different grammars, a naive one which does not obey operator precedence (for demonstration purposes)
+// and an LL1 grammar that implements the true syntax of Amy
+object Parser extends Pipeline[Stream[Token], Program] {
+
+  /* This grammar does not implement the correct syntax of Amy and is not LL1
+   * It is given as an example
+   */
+  val amyGrammar = Grammar('Program, List[Rules[Token]](
+    'Program ::= 'ModuleDefs,
+    'ModuleDefs ::= 'ModuleDef ~ 'ModuleDefs | epsilon(),
+    'ModuleDef ::= OBJECT() ~ 'Id ~ LBRACE() ~ 'Definitions ~ 'OptExpr ~ RBRACE() ~ EOF(),
+    'Definitions ::= 'Definition ~ 'Definitions | epsilon(),
+    'Definition ::= 'AbstractClassDef | 'CaseClassDef | 'FunDef,
+    'AbstractClassDef ::= ABSTRACT() ~ CLASS() ~ 'Id,
+    'CaseClassDef ::= CASE() ~ CLASS() ~ 'Id ~ LPAREN() ~ 'Params ~ RPAREN() ~ EXTENDS() ~ 'Id,
+    'FunDef ::= DEF() ~ 'Id ~ LPAREN() ~ 'Params ~ RPAREN() ~ COLON() ~ 'Type ~ EQSIGN() ~ LBRACE() ~ 'Expr ~ RBRACE(),
+    'Params ::= epsilon() | 'Param ~ 'ParamList,
+    'ParamList ::= epsilon() | COMMA() ~ 'Param ~ 'ParamList,
+    'Param ::= 'Id ~ COLON() ~ 'Type,
+    'OptExpr ::= 'Expr | epsilon(),
+    'Type ::= INT() | STRING() | BOOLEAN() | UNIT() | 'QName,
+    'QName ::= 'Id | 'Id ~ DOT() ~ 'Id,
+    'Expr ::= 'Id | 'Literal | 'Expr ~ 'BinOp ~ 'Expr | BANG() ~ 'Expr | MINUS() ~ 'Expr |
+              'QName ~ LPAREN() ~ 'Args ~ RPAREN() | 'Expr ~ SEMICOLON() ~ 'Expr |
+              VAL() ~ 'Param ~ EQSIGN() ~ 'Expr ~ SEMICOLON() ~ 'Expr |
+              IF() ~ LPAREN() ~ 'Expr ~ RPAREN() ~ LBRACE() ~ 'Expr ~ RBRACE() ~ ELSE() ~ LBRACE() ~ 'Expr ~ RBRACE() |
+              'Expr ~ MATCH() ~ LBRACE() ~ 'Cases ~ RBRACE() |
+              ERROR() ~ LPAREN() ~ 'Expr ~ RPAREN() |
+              LPAREN() ~ 'Expr ~ RPAREN(),
+    'Literal ::= TRUE() | FALSE() | LPAREN() ~ RPAREN() | INTLITSENT | STRINGLITSENT,
+    'BinOp ::= PLUS() | MINUS() | TIMES() | DIV() | MOD() | LESSTHAN() | LESSEQUALS() |
+               AND() | OR() | EQUALS() | CONCAT(),
+    'Cases ::= 'Case | 'Case ~ 'Cases,
+    'Case ::= CASE() ~ 'Pattern ~ RARROW() ~ 'Expr,
+    'Pattern ::= UNDERSCORE() | 'Literal | 'Id | 'QName ~ LPAREN() ~ 'Patterns ~ RPAREN(),
+    'Patterns ::= epsilon() | 'Pattern ~ 'PatternList,
+    'PatternList ::= epsilon() | COMMA() ~ 'Pattern ~ 'PatternList,
+    'Args ::= epsilon() | 'Expr ~ 'ExprList,
+    'ExprList ::= epsilon() | COMMA() ~ 'Expr ~ 'ExprList,
+    'Id ::= IDSENT
+  ))
+
+  // TODO: Write a grammar that implements the correct syntax of Amy and is LL1.
+  // You can start from the example above and work your way from there.
+  // Make sure you use the warning (see `run` below) that tells you which part is not in LL1.
+  lazy val amyGrammarLL1 = Grammar('Program, List[Rules[Token]](
+    'Program ::= 'ModuleDefs,
+    'ModuleDefs ::= 'ModuleDef ~ 'ModuleDefs | epsilon(),
+    'ModuleDef ::= OBJECT() ~ 'Id ~ LBRACE() ~ 'Definitions ~ 'OptExpr ~ RBRACE() ~ EOF(),
+    'Definitions ::= 'Definition ~ 'Definitions | epsilon(),
+    'Definition ::= 'AbstractClassDef | 'CaseClassDef | 'FunDef,
+    'AbstractClassDef ::= ABSTRACT() ~ CLASS() ~ 'Id,
+    'CaseClassDef ::= CASE() ~ CLASS() ~ 'Id ~ LPAREN() ~ 'Params ~ RPAREN() ~ EXTENDS() ~ 'Id,
+
+
+    'FunDef ::= 'Op ~ DEF() ~ 'Id ~ LPAREN() ~ 'Params ~ RPAREN() ~ COLON() ~ 'Type ~ EQSIGN() ~ LBRACE() ~ 'Expr ~ RBRACE(),
+
+
+    'Params ::= epsilon() | 'Param ~ 'ParamList,
+    'ParamList ::= epsilon() | COMMA() ~ 'Param  ~ 'ParamList,
+    'Param ::= 'Id ~ COLON() ~ 'Type,
+    'OptExpr ::= 'Expr | epsilon(),    'Op ::=  OPERATOR() | epsilon(),
+
+    'ExprTail ::= SEMICOLON() ~ 'Expr | epsilon(),
+    'Type ::= INT() | STRING() | BOOLEAN() | UNIT() | 'QName,
+    'QName ::= 'Id ~ 'QNames,
+    'QNames ::= DOT() ~ 'Id | epsilon(),
+    'Expr ::= VAL() ~ 'Param ~ EQSIGN() ~ 'ExprTerm ~ SEMICOLON() ~  'Expr
+      |'ExprTerm ~ 'ExprTail,
+
+    //'ExprTerm ::= 'OptMatch ~ 'OrTerm ,
+
+    'ExprTerm ::= 'OrTerm ~ 'OptMatch,
+    'OptMatch ::= MATCH() ~ LBRACE() ~ 'Cases ~ RBRACE()| epsilon(),
+    'OrTermList ::=  OR() ~  'OrTerm  | epsilon(),
+    'OrTerm ::= 'AndTerm ~ 'OrTermList,
+
+    'AndTermList ::= AND() ~ 'AndTerm | epsilon(),
+    'AndTerm ::=  'EqTerm ~ 'AndTermList,
+
+    'EqTermList ::= EQUALS() ~'EqTerm | epsilon(),
+    'EqTerm ::= 'LessTerm ~ 'EqTermList,
+
+    'LessTermList ::= 'LESS ~ 'LessTerm| epsilon(),
+    'LessTerm ::= 'Plus_MinusTerm ~ 'LessTermList,
+
+    'Plus_MinusTermList ::= 'PLUS_MINUS ~ 'Plus_MinusTerm | epsilon(),
+    'Plus_MinusTerm ::='MUL_DIV_MODTerm ~ 'Plus_MinusTermList,
+
+    'MUL_DIV_MODTermList ::= 'MUL_DIV_MOD ~ 'MUL_DIV_MODTerm | epsilon(),
+    'MUL_DIV_MODTerm ::= 'LastLevel ~ 'MUL_DIV_MODTermList,
+
+    'LastLevelList ::= 'Id ~ 'LastLevel | epsilon(),
+    'LastLevel ::= 'FinalTerm ~ 'LastLevelList,
+
+     'FinalTerm ::= 'If | 'Error | 'Id ~ 'OptCall |'LiteralNoEmptyPar | 'EmptyParOrParExpr
+      | BANG() ~ 'FinalTerm | MINUS() ~ 'FinalTerm,
+
+    'OptForQname ::= DOT() ~ 'Id | epsilon(),
+    'OptForUnary ::= 'UNARY | epsilon(),
+    'OptCall ::= 'OptForQname ~ LPAREN() ~ 'Args ~ RPAREN() | epsilon(),
+    'ParExpr ::=  LPAREN() ~ 'Expr ~ RPAREN(),
+    'Val ::= 'Id,
+    'Call ::= 'QName ~ LPAREN() ~ 'Args ~ RPAREN(),
+    'Error ::= ERROR() ~ LPAREN() ~ 'Expr ~ RPAREN(),
+    'If ::= IF() ~ LPAREN() ~ 'Expr ~ RPAREN() ~ LBRACE() ~ 'Expr ~ RBRACE() ~ ELSE() ~ LBRACE() ~ 'Expr ~ RBRACE(),
+
+    'UNARY ::= BANG() ~ 'FinalTerm | MINUS() ~ 'FinalTerm | 'Id ~ 'FinalTerm,
+    //UNARY ::= 'Id ~ 'FinalTerm,
+    'PLUS_MINUS ::= PLUS() | MINUS() | CONCAT(),
+    'MUL_DIV_MOD ::= TIMES() | DIV() | MOD(),
+    'LESS ::= LESSTHAN() | LESSEQUALS(),
+
+    'Literal ::= TRUE() | FALSE() | LPAREN() ~ RPAREN() | INTLITSENT | STRINGLITSENT,
+    'LiteralNoEmptyPar ::=  TRUE() | FALSE() | INTLITSENT | STRINGLITSENT,
+    'EmptyParOrParExpr ::= LPAREN() ~ 'OptExpr  ~ RPAREN(),
+
+    'Cases ::= 'Case ~ 'CasesTail,
+    'CasesTail ::=  'Cases | epsilon(),
+    'Case ::= CASE() ~ 'Pattern ~ RARROW() ~ 'Expr,
+    'Pattern ::= UNDERSCORE()  | 'IdOrQnameWithPattern | 'Literal ,
+
+    'IdOrQnameWithPattern ::= 'QName ~ 'OptPatCall,
+    'Patterns ::= 'Pattern ~ 'PatternList,
+    'OptPattern ::= 'Pattern | epsilon(),
+
+    'OptPatCall ::= LPAREN() ~ 'OptPatterns  ~ RPAREN()| epsilon(),
+    'OptPatterns ::= 'Patterns | epsilon(),
+    'PatternList ::= epsilon() | COMMA() ~'Patterns,
+
+    'Args ::= epsilon() | 'Expr ~ 'ExprList,
+    'ExprList ::= epsilon() | COMMA() ~ 'Expr ~ 'ExprList,
+    'Id ::= IDSENT
+  ))
+
+  def run(ctx: Context)(tokens: Stream[Token]): Program = {
+    // TODO: Switch to LL1 when you are ready
+    val (grammar, constructor) = (amyGrammarLL1, new ASTConstructorLL1)
+   // val (grammar, constructor) = (amyGrammar, new ASTConstructor)
+
+    import ctx.reporter._
+    implicit val gc = new GlobalContext()
+    implicit val pc = new ParseContext()
+
+    GrammarUtils.isLL1WithFeedback(amyGrammarLL1) match {
+      case InLL1() =>
+        info("Grammar is in LL1")
+      case other =>
+        warning(other)
+    }
+
+    val feedback = ParseTreeUtils.parseWithTrees(grammar, tokens.toList)
+    feedback match {
+      case s: Success[Token] =>
+        constructor.constructProgram(s.parseTrees.head)
+      case err@LL1Error(_, Some(tok)) =>
+        fatal(s"Parsing failed: $err", tok.obj.position)
+      case err =>
+        fatal(s"Parsing failed: $err")
+    }
+  }
+
+}
